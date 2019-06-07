936bf5394a2a82bde699473a6ae28685941d9e45
GalBrandwine <gal080592@gmail.com>
2019-06-07 14:28:35 +0300

    working on GUI, added predesigned gui

--- GUI/tutorial_2.py
+++ GUI/tutorial_2.py
@@ -1,31 +1,60 @@
-import tkinter as tk
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
 
-def startgame():
+class Calculator:
 
-    pass
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
 
-mw = tk.Tk()
+        self.total = 0
+        self.entered_number = 0
 
-#If you have a large number of widgets, like it looks like you will for your
-#game you can specify the attributes for all widgets simply like this.
-mw.option_add("*Button.Background", "black")
-mw.option_add("*Button.Foreground", "red")
+        self.total_label_text = IntVar()
+        self.total_label_text.set(self.total)
+        self.total_label = Label(master, textvariable=self.total_label_text)
 
-mw.title('The game')
-#You can set the geometry attribute to change the root windows size
-mw.geometry("500x500") #You want the size of the app to be 500x500
-mw.resizable(0, 0) #Don't allow resizing in the x or y direction
+        self.label = Label(master, text="Total:")
 
-back = tk.Frame(master=mw,bg='black')
-back.pack_propagate(0) #Don't allow the widgets inside to determine the frame's width / height
-back.pack(fill=tk.BOTH, expand=1) #Expand the frame to fill the root window
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
 
-#Changed variables so you don't have these set to None from .pack()
-go = tk.Button(master=back, text='Start Game', command=startgame)
-go.pack()
-close = tk.Button(master=back, text='Quit', command=mw.destroy)
-close.pack()
-info = tk.Label(master=back, text='Made by me!', bg='red', fg='black')
-info.pack()
+        self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+        self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
+        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
 
-mw.mainloop()
+        # LAYOUT
+
+        self.label.grid(row=0, column=0, sticky=W)
+        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
+
+        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+        self.add_button.grid(row=2, column=0)
+        self.subtract_button.grid(row=2, column=1)
+        self.reset_button.grid(row=2, column=2, sticky=W+E)
+
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.entered_number = 0
+            return True
+
+        try:
+            self.entered_number = int(new_text)
+            return True
+        except ValueError:
+            return False
+
+    def update(self, method):
+        if method == "add":
+            self.total += self.entered_number
+        elif method == "subtract":
+            self.total -= self.entered_number
+        else: # reset
+            self.total = 0
+
+        self.total_label_text.set(self.total)
+        self.entry.delete(0, END)
+
+root = Tk()
+my_gui = Calculator(root)
+root.mainloop()
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 B.S.c Final project - Verification and generation tools for Automatons 
 
 # GUI
-Project's gui will follow a minimalistic design.
+Project's gui will follow a minimalistic design. 
+Gui was pre-designed using [this site](https://designer.gravit.io/?d=UGo_UmGpW)
